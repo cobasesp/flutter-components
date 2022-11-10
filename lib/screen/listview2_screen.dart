0faccbfae5137012c3899944ee85ab65d9e5dd15
David Cobas <cobasesp.developer@gmail.com>
2022-11-10 18:11:23 +0100

    onTap y colores

--- lib/screen/listview2_screen.dart
+++ lib/screen/listview2_screen.dart
@@ -14,10 +14,20 @@ class Listview2Screen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text("Listview 2")),
+        appBar: AppBar(
+          title: const Text("Listview 2"),
+          backgroundColor: Colors.indigo,
+        ),
         // Se usa el separator para poner un divider ye lbuilder es igual pero sin separador
         body: ListView.separated(
-          itemBuilder: ((context, i) => ListTile(title: Text(games[i]), trailing: const Icon(Icons.keyboard_arrow_right),)), 
+          itemBuilder: ((context, i) => ListTile(
+            title: Text(games[i]), 
+            trailing: const Icon(Icons.keyboard_arrow_right, color: Colors.indigo),
+            onTap: () {
+              final game = games[i];
+              print(game);
+            }  
+          )), 
           separatorBuilder: ((context, index) => const Divider()), 
           itemCount: games.length,
         )
