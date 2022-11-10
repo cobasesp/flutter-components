412737ca28ff53e9844cb8f4858f147b1f9f8991
David Cobas <cobasesp.developer@gmail.com>
2022-11-10 17:44:24 +0100

    ListTiles y listas

--- lib/screen/listview1_screen.dart
+++ lib/screen/listview1_screen.dart
@@ -3,20 +3,23 @@ import 'package:flutter/material.dart';
 class Listview1Screen extends StatelessWidget {
   const Listview1Screen({Key? key}) : super(key: key);
 
+  final games = const [
+    "Megaman",
+    "Metal gear",
+    "Hunt",
+    "League of legends",
+    "Valorant"
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: const Text("Listview 1")),
         body: ListView(children: [
-          Text('Texto 1'),
-          Text('Texto 1'),
-          Text('Texto 1'),
-          Text('Texto 1'),
-          Text('Texto 1'),
-          Text('Texto 1'),
-          Text('Texto 1'),
-          Text('Texto 1'),
-          Text('Texto 1'),
+          ...games
+              .map((e) =>
+                  ListTile(leading: Icon(Icons.verified_user), title: Text(e)))
+              .toList()
         ]));
   }
 }
