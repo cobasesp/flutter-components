9942253e82542f4c7eb5b8f6ff075547192db55f
David Cobas <cobasesp.developer@gmail.com>
2022-11-27 14:16:28 +0100

    Clase menuoptions para crear de manera sencilla mas links para el router

--- lib/router/app_routes.dart
+++ lib/router/app_routes.dart
@@ -1,8 +1,17 @@
+import 'package:components/models/models.dart';
 import 'package:flutter/material.dart';
 import '../screen/screen.dart';
 
 class AppRoutes {
   static const initialRoute = 'home';
+
+  static final menuOptions = <MenuOption>[
+    // TODO: borrar home
+    MenuOption(route: 'home', icon: Icons.home, name: 'Home Screen', screen: const HomeScreen()),
+    MenuOption(route: 'listview1', icon: Icons.list, name: 'List View 1', screen: const Listview1Screen()),
+    MenuOption(route: 'listview2', icon: Icons.list_rounded, name: 'List View 2', screen: const Listview2Screen()),
+    MenuOption(route: 'alert', icon: Icons.add_alert_outlined, name: 'Alerts', screen: const AlertScreen()),
+  ];
 
   static Map<String, Widget Function(BuildContext)> routes = {
     'home': ((context) => const HomeScreen()),
